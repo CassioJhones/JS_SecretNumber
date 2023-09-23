5287c6c35d9af8c6f1288dca956565b619f442a2
Cassio Jhones <56178855+CassioJhones@users.noreply.github.com>
2023-09-23 02:01:22 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -7,51 +7,41 @@ function verificaChute(chute) {
       document.body.innerHTML = `
           <h2>Game Over!!!</h2>
           <h3>Pressione o botão para jogar novamente</h3>
-          <button id="jogar-denovo" class="btn-jogar" >Jogar novamente</button>
-          `;
-      document.body.style.backgroundColor = "black";
-    } else {
-      elementoChute.innerHTML += "<div>Valor Inválido</div>";
-    }
+          <button id="jogar-denovo" class="btn-jogar" >Jogar novamente</button> `;
+      document.body.style.backgroundColor = "black"; } 
+    else {
+      elementoChute.innerHTML += "<div>Valor Inválido</div>";  }
     return;
   }
 
   if (numeroLimite(numero)) {
     elementoChute.innerHTML += `
-    <div> Número fora do limite: ${menorValor} e ${maiorValor} </div>
-      `;
+    <div> Número fora do limite: ${menorValor} e ${maiorValor} </div> `;
     return;
   }
 
-  // vaidacao para quando o usuario acertar o numero
+  // validacao para quando o usuario acertar o numero
   // body redesenha a tela apagando o conteudo anterior
   if (numero === numeroSecreto) {
     document.body.innerHTML = `
     <h2> Você Acertou ! </h2>
     <h3> O número secreto era ${numeroSecreto}</h3>
-    <button id="jogar-denovo" class="btn-jogar">Jogar Novamente</button>
-  `;
+    <button id="jogar-denovo" class="btn-jogar">Jogar Novamente</button>  `;
+    
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `
-    <div>Tente um número menor <i class="fa-solid fa-circle-down" style="color: #FF6D60;"></i></div>
-    `;
-  } else {
+    <div>Tente um número menor <i class="fa-solid fa-circle-down" style="color: #FF6D60;"></i></div>  `;  } 
+  else {
     elementoChute.innerHTML += `
-    <div>Tente um número maior <i class="fa-solid fa-circle-up" style="color: #FF6D60;"></i></div>`;
-  }
-}
+    <div>Tente um número maior <i class="fa-solid fa-circle-up" style="color: #FF6D60;"></i></div>`;  }}
 
 function numeroLimite(numero) {
-  return numero > maiorValor || numero < menorValor;
-}
+  return numero > maiorValor || numero < menorValor;}
 
 function chuteInvalido(numero) {
-  return Number.isNaN(numero);
-}
+  return Number.isNaN(numero);}
 
 // adiciona um evento ao botao, faz a tela ser recarregada ao clicar
 document.body.addEventListener("click", (e) => {
   if (e.target.id == "jogar-denovo") {
-    window.location.reload();
-  }
-});
+    window.location.reload();  }});
